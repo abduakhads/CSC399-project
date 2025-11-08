f2a5d3e5d60b9078e8f90307b6ab49973f8aecfe
abduakhads <160651255+abduakhads@users.noreply.github.com>
2025-11-08 15:02:36 +0300

    initial commit

---
+++ src/main/java/com/example/csc399project/Csc399ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.csc399project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csc399ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Csc399ProjectApplication.class, args);
+    }
+
+}
